4f23828e2c388fa47db7b5d3397d53a97de007a0
Dominic Bett <dominicbett@hotmail.com>
2015-12-02 15:01:16 -0500

    version 1.0

---
+++ README.md
@@ -0,0 +1,2 @@
+# Securus
+iOS app to report on real-time security threats in the community.
---
+++ Securus/HomeViewController.swift
@@ -0,0 +1,73 @@
+//
+//  TimelineViewController.swift
+//  Securus
+//
+//  Created by Dominic Bett on 11/16/15.
+//  Copyright © 2015 DominicBett. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class HomeViewController: UIViewController {
+    @IBOutlet weak var userNameLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Show the current visitor's username
+        if let pUserName = PFUser.currentUser()?["username"] as? String {
+            self.userNameLabel.text = "@" + pUserName
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func unwindToHomeScreen(segue:UIStoryboardSegue) {
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        if (PFUser.currentUser() == nil) {
+            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                
+                let viewController:UIViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("Login")
+                self.presentViewController(viewController, animated: true, completion: nil)
+            })
+        }
+    }
+    
+    @IBAction func logOutAction(sender: AnyObject){
+        
+        // Send a request to log out a user
+        PFUser.logOut()
+        
+        dispatch_async(dispatch_get_main_queue(), { () -> Void in
+            let viewController:UIViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("Login")
+            self.presentViewController(viewController, animated: true, completion: nil)
+        })
+        
+    }
+    
+    // MARK: Button to open Event Post Page
+    @IBAction func goPostEvent(sender: AnyObject) {
+        self.performSegueWithIdentifier("compose", sender: self)
+    }
+    
+    @IBAction func goTimeline(sender: AnyObject) {
+        self.performSegueWithIdentifier("timeline", sender: self)
+    }
+    
+    /*
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
+    }
+    */
+    
+}
