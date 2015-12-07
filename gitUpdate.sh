52adda28e9cdfeda962936e4f18b44519690aa4e
Dominic Bett <dominicbett@hotmail.com>
2015-12-06 19:28:24 -0500

    123456

--- gitUpdate.sh
+++ gitUpdate.sh
@@ -4,8 +4,7 @@
 #!/bin/bash
 git pull
 git add -A
-printf "Please enter the commit message:\n "
-read -r commit_message 
-printf $commit_message
+read -p "Please enter the commit message: " commit_message 
+echo "Commit message is - `$commit_message`."
 git commit -m $commit_message
 git push -u origin master
