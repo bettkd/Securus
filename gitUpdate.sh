75f490c65ad598b9820c9df0d9a60dd1f88b2a92
Dominic Bett <dominicbett@hotmail.com>
2015-12-06 19:41:16 -0500

    tiny change

--- gitUpdate.sh
+++ gitUpdate.sh
@@ -1,10 +1,9 @@
 # A shell script which when run updates git
 # Usage: ./gitUpdate.sh
-
 #!/bin/bash
 git pull
 git add -A
 read -e -p "Please enter the commit message: " commit_message 
 echo "Commit message is - $commit_message."
-git commit -m $commit_message
+git commit -m "$commit_message"
 git push -u origin master
