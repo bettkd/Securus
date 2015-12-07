e5c9bdfef6398a620a679b6412b12b5891f56b93
Dominic Bett <dominicbett@hotmail.com>
2015-12-06 19:39:10 -0500

    ggg

--- gitUpdate.sh
+++ gitUpdate.sh
@@ -4,7 +4,7 @@
 #!/bin/bash
 git pull
 git add -A
-read -p "Please enter the commit message: " commit_message 
+read -e -p "Please enter the commit message: " commit_message 
 echo "Commit message is - $commit_message."
 git commit -m $commit_message
 git push -u origin master
