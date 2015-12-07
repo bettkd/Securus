011faca3e833b98e81f3d199f0c12a2aa3f63b31
Dominic Bett <dominicbett@hotmail.com>
2015-12-06 19:29:40 -0500

    sdkjsdlkvnd

--- gitUpdate.sh
+++ gitUpdate.sh
@@ -5,6 +5,6 @@
 git pull
 git add -A
 read -p "Please enter the commit message: " commit_message 
-echo "Commit message is - `$commit_message`."
+echo "Commit message is - $commit_message."
 git commit -m $commit_message
 git push -u origin master
